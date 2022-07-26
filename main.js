509e559aa7244fbf2f4c3a5c01d4726bc0914b5c
Nikita Tundra <tundra-s@mail.ru>
2022-07-26 16:41:36 +0300

    feat: Inint render

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const context = {};
+
+const getContext = () => {
+  return context.ctx;
+};
+
+const initContext = () => {
+  context.canvas = document.querySelector("#canvas");
+  context.ctx = context.canvas && context.canvas.getContext("2d");
+};
+
+const draw = () => {};
+
+const render = () => {
+  draw();
+
+  window.requestAnimationFrame(render);
+};
+
+const init = () => {
+  initContext();
+  window.requestAnimationFrame(render);
+};
+
+window.onload = init;
